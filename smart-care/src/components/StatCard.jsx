5e4632870b6acc4febbc9836e3aed141db264e3d
Sushi-Maru <sikkadit.00@gmail.com>
2026-03-05 08:18:37 +0700

    Remove some useless

--- smart-care/src/components/StatCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value, color }) {
-  return (
-    <div className="bg-white p-5 rounded-2xl shadow">
-      <h2 className="text-gray-500 text-sm">{title}</h2>
-      <p className={`text-3xl font-bold ${color}`}>{value}</p>
-    </div>
-  )
-}
